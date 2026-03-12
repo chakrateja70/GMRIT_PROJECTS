9f47abc04d2743475992b7179ec4353f5c761566
teja <tejapolamarasetty2004@gmail.com>
2026-03-12 11:44:56 +0530

    deleted RAG-Assist

--- RAG-Assist/src/core/constants.py
+++
@@ -1,6 +0,0 @@
-# src/core/constants.py
-
-FALLBACK_MESSAGE = (
-    "I can only answer questions based on the information I have. "
-    "For any other inquiries, please contact the team directly at contact@apmedicalcouncil.in."
-)
